04283263273e11af6ed3289c2021d1f4864ec49a
Peter Minns <peter.minns@asda.co.uk>
2023-10-26 11:54:26 +0100

    Add Fizz Buzz exercise

---
+++ Fizz Buzz Exercise 1.py
@@ -0,0 +1,16 @@
+number_range = range(1,101)
+
+for number in number_range:
+    if number % 3 == 0 and number % 5 == 0:
+        print ('FizzBuzz')
+        continue
+    elif number % 3 == 0:
+        print ('Fizz')
+        continue
+    elif number % 5 == 0:
+        print ('Buzz')
+        continue
+    else:
+        print(number)
+
+        
